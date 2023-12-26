b129fd14898c6c9fb59509e063f3287999287864
Baharudin Yusup <baharudin.yusup@outlook.com>
2023-12-26 16:28:44 +0700

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "dev.baharudin.themoviedb"
         minSdk = 31
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
