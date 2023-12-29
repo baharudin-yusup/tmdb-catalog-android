2e55ca84fbb5399507f8ef48c550a1069007a735
Baharudin Yusup <baharudin.yusup@outlook.com>
2023-12-29 07:24:36 +0700

    Minor update: increment app version and rename Versions.GRADLE to Versions.AGP (#28)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,10 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version Versions.GRADLE apply false
+    id("com.android.application") version Versions.AGP apply false
     id("org.jetbrains.kotlin.android") version Versions.KOTLIN apply false
     id("com.google.dagger.hilt.android") version Versions.HILT apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
-    id("com.android.library") version Versions.GRADLE apply false
-    id("com.android.dynamic-feature") version Versions.GRADLE apply false
+    id("com.android.library") version Versions.AGP apply false
+    id("com.android.dynamic-feature") version Versions.AGP apply false
 }
