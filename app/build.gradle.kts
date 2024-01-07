df8d5a3fbde08d87f3d2e3a4d2033c465a2dbcbc
Baharudin Yusup <baharudin.yusup@outlook.com>
2024-01-08 00:50:19 +0700

    Patch/update app version to 1.2.0 (#34)
    * Update build.gradle.kts
    
    * Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "dev.baharudin.tmdb_android"
         minSdk = 30
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.1"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
